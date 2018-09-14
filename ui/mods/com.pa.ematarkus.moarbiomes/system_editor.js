35e42c56bc4469075af6c1f6d63b1119cdb0e33b
ematarkus <ematarkus@eserver.homelinux.org>
2018-09-14 14:27:31 +0200

    First commit

---
+++ ui/mods/com.pa.ematarkus.moarbiomes/system_editor.js
@@ -0,0 +1,7 @@
+model.biomes.push('earth_lava');
+model.biomes.push('earth_lava_metal');
+model.biomes.push('garden');
+model.biomes.push('grass_metal');
+model.biomes.push('green_moon');
+model.biomes.push('metal_water');
+model.biomes.push('metallic_moon');
